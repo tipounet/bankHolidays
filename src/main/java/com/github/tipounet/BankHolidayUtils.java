a6755f8c8f933afd6f74d41e3acbbac905637004
Guillaume Laplace <glaplace@ausy-group.com>
2019-09-25 22:41:33 +0200

    passe technique
    mise à jour de doc
    .editorconfig

--- src/main/java/com/github/tipounet/BankHolidayUtils.java
+++ src/main/java/com/github/tipounet/BankHolidayUtils.java
@@ -6,161 +6,164 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Gestions des jours feries Francais
  */
-public class BankHolidayUtils {
+public final class BankHolidayUtils {
 
-  private List<LocalDate> fixedDay;
+    private List<LocalDate> fixedDay;
 
-  private void initFixedDay(final int year) {
-    LocalDate easterDay = getEasterDay(year);
-    fixedDay = Lists.newArrayList(LocalDate.of(year, Month.JANUARY, 1),
-        LocalDate.of(year, Month.MAY, 1),
-        LocalDate.of(year, Month.MAY, 8),
-        LocalDate.of(year, Month.JULY, 14),
-        LocalDate.of(year, Month.AUGUST, 15),
-        LocalDate.of(year, Month.NOVEMBER, 1),
-        LocalDate.of(year, Month.NOVEMBER, 11),
-        LocalDate.of(year, Month.DECEMBER, 25),
-        LocalDate.of(year, Month.DECEMBER, 31),
-        easterDay,
-        getAscension(easterDay),
-        getMondayOfEasterDay(easterDay),
-        getMondayOfPentecote(easterDay)
-    );
-  }
-
-  /**
-   * Calcul le jour de paques pour l'annee
-   * <p>
-   * Algorithme de Butcher, méthode de Meeus
-   * https://fr.wikipedia.org/wiki/Calcul_de_la_date_de_P%C3%A2ques_selon_la_m%C3%A9thode_de_Meeus#Calcul_de_la_date_de_P.C3.A2ques_gr.C3.A9gorienne
-   *
-   * @param year
-   * @return
-   */
-  LocalDate getEasterDay(final int year) {
-
-    final int cycleMeton = year % 19;
-    final int centaine = year / 100;
-    final int rang = year % 100;
-
-    final int siecleBissextile = centaine / 4;
-    final int siecleBissextileReste = centaine % 4;
-    final int cycleProemptose = (centaine + 8) / 25;
-
-    final int proemptose = (centaine - cycleProemptose + 1) / 3;
-    final int epacte = (19 * cycleMeton + centaine - siecleBissextile - proemptose + 15) % 30;
-    final int anneeBissextile = rang / 4;
-    final int anneeBissextileReste = rang % 4;
-    final int lettreDominicale = (32 + 2 * siecleBissextileReste + 2 * anneeBissextile - epacte - anneeBissextileReste) % 7;
-    final int correction = (cycleMeton + 11 * epacte + 22 * lettreDominicale) / 451;
-    final int tmp = epacte + lettreDominicale - 7 * correction + 114;
-    final int m = tmp / 31;
-    final int day = (tmp % 31) + 1;
-
-    Month month;
-    if (m == 3) {
-      month = Month.MARCH;
-    } else {
-      month = Month.APRIL;
-    }
-    return LocalDate.of(year, month, day);
-  }
-
-  /**
-   * Calcul le jour de pâque pour l'année en paramètre avec la méthode de Conway
-   * https://fr.wikipedia.org/wiki/Calcul_de_la_date_de_P%C3%A2ques_selon_la_m%C3%A9thode_de_Conway
-   *
-   * @param year
-   * @return
-   */
-  LocalDate getEasterDayConwayMethod(final int year) {
-    final int s = year / 100;//annnée séculaire
-    final int t = year % 100; // millésime
-    final int a = t / 4;// terme bissextil
-    final int p = s % 4;
-    final int jps = (9 - 2 * p) % 7; // jour pivot séculaire
-    final int jp = (jps + t + a) % 7; // jout-pivot de l'année courante
-    final int g = year % 19;
-    final int G = g + 1; // Cycle de Méton
-    final int b = s / 4; // Métemptose
-    final int r = (8 * (s + 11)) / 25; //Proemptose
-
-    final int C = -s + b + r;//Correction séculaire
-
-    final int d = (((11 * G + C) % 30) + 30) % 30;//Pleine lune pascale
-
-    final int h = (551 - 19 * d + G) / 544;// Correction des exceptions à l'épacte
-
-    final int e = (50 - d - h) % 7;// écart de la pleine lune pascale au jour-pivot
-    final int f = (e + jp) % 7; // jour de la plene lune pascale
-
-    final int R = 57 - d - f - h; // Dimanche de paques
-    int day;
-    Month month;
-    if (R <= 31) {
-      day = R;
-      month = Month.MARCH;
-    } else {
-      month = Month.APRIL;
-      day = R - 31;
+    private void initFixedDay(final int year) {
+        LocalDate easterDay = getEasterDay(year);
+        fixedDay = Lists.newArrayList(LocalDate.of(year, Month.JANUARY, 1),
+                LocalDate.of(year, Month.MAY, 1),
+                LocalDate.of(year, Month.MAY, 8),
+                LocalDate.of(year, Month.JULY, 14),
+                LocalDate.of(year, Month.AUGUST, 15),
+                LocalDate.of(year, Month.NOVEMBER, 1),
+                LocalDate.of(year, Month.NOVEMBER, 11),
+                LocalDate.of(year, Month.DECEMBER, 25),
+                LocalDate.of(year, Month.DECEMBER, 31),
+                easterDay,
+                getAscension(easterDay),
+                getMondayOfEasterDay(easterDay),
+                getMondayOfPentecote(easterDay)
+        );
     }
 
-    return LocalDate.of(year, month, day);
-  }
+    /**
+     * Calcul le jour de pâques pour l'annee {@code year}
+     * <p>
+     * Algorithme de Butcher, méthode de Meeus
+     * https://fr.wikipedia.org/wiki/Calcul_de_la_date_de_P%C3%A2ques_selon_la_m%C3%A9thode_de_Meeus#Calcul_de_la_date_de_P.C3.A2ques_gr.C3.A9gorienne
+     *
+     * @param year année dans laquelle on souhaite le jour de pâques
+     * @return le jour de pâques
+     */
+    LocalDate getEasterDay(final int year) {
 
-  /**
-   * lundi de paques
-   *
-   * @param easterDay
-   * @return
-   */
-  LocalDate getMondayOfEasterDay(LocalDate easterDay) {
-    assert easterDay != null;
-    return easterDay.plus(1, ChronoUnit.DAYS);
-  }
+        final int cycleMeton = year % 19;
+        final int centaine = year / 100;
+        final int rang = year % 100;
 
-  /**
-   * jeudi de l'ascencion
-   *
-   * @param easterDay
-   * @return
-   */
-  LocalDate getAscension(LocalDate easterDay) {
-    assert easterDay != null;
-    return easterDay.plus(39, ChronoUnit.DAYS);
-  }
+        final int siecleBissextile = centaine / 4;
+        final int siecleBissextileReste = centaine % 4;
+        final int cycleProemptose = (centaine + 8) / 25;
 
-  /**
-   * Lundi de pentecote
-   *
-   * @param easterDay
-   * @return
-   */
-  LocalDate getMondayOfPentecote(LocalDate easterDay) {
-    assert easterDay != null;
-    return easterDay.plus(50, ChronoUnit.DAYS);
-  }
+        final int proemptose = (centaine - cycleProemptose + 1) / 3;
+        final int epacte = (19 * cycleMeton + centaine - siecleBissextile - proemptose + 15) % 30;
+        final int anneeBissextile = rang / 4;
+        final int anneeBissextileReste = rang % 4;
+        final int lettreDominicale = (32 + 2 * siecleBissextileReste + 2 * anneeBissextile - epacte - anneeBissextileReste) % 7;
+        final int correction = (cycleMeton + 11 * epacte + 22 * lettreDominicale) / 451;
+        final int tmp = epacte + lettreDominicale - 7 * correction + 114;
+        final int m = tmp / 31;
+        final int day = (tmp % 31) + 1;
 
-  /**
-   * Verifie si la date ets un jour ferie.
-   *
-   * @param date
-   * @return
-   */
-  public boolean isBankHoliday(LocalDate date) {
-    assert date != null;
-    initFixedDay(date.getYear());
-    boolean retour = false;
-    for (LocalDate l : fixedDay) {
-      if (l.isEqual(date)) {
-        retour = true;
-        break;
-      }
+        Month month;
+        if (m == 3) {
+            month = Month.MARCH;
+        } else {
+            month = Month.APRIL;
+        }
+        return LocalDate.of(year, month, day);
     }
-    return retour;
-  }
+
+    /**
+     * Calcul le jour de pâque pour l'année en paramètre avec la méthode de Conway
+     * https://fr.wikipedia.org/wiki/Calcul_de_la_date_de_P%C3%A2ques_selon_la_m%C3%A9thode_de_Conway
+     *
+     * @param year année dont l'on souhaite trouver le jour de pâque
+     * @return le jour de pâque pour l'année {@code year}
+     */
+    LocalDate getEasterDayConwayMethod(final int year) {
+        final int s = year / 100;//annnée séculaire
+        final int t = year % 100; // millésime
+        final int a = t / 4;// terme bissextil
+        final int p = s % 4;
+        final int jps = (9 - 2 * p) % 7; // jour pivot séculaire
+        final int jp = (jps + t + a) % 7; // jout-pivot de l'année courante
+        final int g = year % 19;
+        final int G = g + 1; // Cycle de Méton
+        final int b = s / 4; // Métemptose
+        final int r = (8 * (s + 11)) / 25; //Proemptose
+
+        final int C = -s + b + r;//Correction séculaire
+
+        final int d = (((11 * G + C) % 30) + 30) % 30;//Pleine lune pascale
+
+        final int h = (551 - 19 * d + G) / 544;// Correction des exceptions à l'épacte
+
+        final int e = (50 - d - h) % 7;// écart de la pleine lune pascale au jour-pivot
+        final int f = (e + jp) % 7; // jour de la plene lune pascale
+
+        final int R = 57 - d - f - h; // Dimanche de pâques
+        int day;
+        Month month;
+        if (R <= 31) {
+            day = R;
+            month = Month.MARCH;
+        } else {
+            month = Month.APRIL;
+            day = R - 31;
+        }
+
+        return LocalDate.of(year, month, day);
+    }
+
+    /**
+     * lundi de pâques
+     *
+     * @param easterDay date de pâques
+     * @return lundi de paque
+     */
+    LocalDate getMondayOfEasterDay(final LocalDate easterDay) {
+        assert easterDay != null;
+        return easterDay.plus(1, ChronoUnit.DAYS);
+    }
+
+    /**
+     * jeudi de l'ascencion
+     *
+     * @param easterDay date de pâques²
+     * @return date du jeudi de pentecote
+     */
+    LocalDate getAscension(final LocalDate easterDay) {
+        assert easterDay != null;
+        return easterDay.plus(39, ChronoUnit.DAYS);
+    }
+
+    /**
+     * Lundi de pentecote
+     *
+     * @param easterDay date de paque
+     * @return lundi de pentecote
+     */
+    LocalDate getMondayOfPentecote(final LocalDate easterDay) {
+        assert easterDay != null;
+        return easterDay.plus(50, ChronoUnit.DAYS);
+    }
+
+    /**
+     * Verifie si la date est un jour férie.
+     *
+     * @param date la date à tester
+     * @return {@code true} si la date est un jour fériée
+     */
+    public boolean isBankHoliday(final LocalDate date) {
+        if (Objects.isNull(date)) {
+            throw new IllegalArgumentException("La date ne peut être null");
+        }
+        initFixedDay(date.getYear());
+        boolean retour = false;
+        for (LocalDate l : fixedDay) {
+            if (l.isEqual(date)) {
+                retour = true;
+                break;
+            }
+        }
+        return retour;
+    }
 }
--- src/test/java/com/github/tipounet/BankHolidayUtilsTest.java
+++ src/test/java/com/github/tipounet/BankHolidayUtilsTest.java
@@ -11,93 +11,93 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BankHolidayUtilsTest {
 
-  private static final int YEAR = 2017;
-  private static final int EXPECTED_DAY_OF_MONTH = 16;
-  private LocalDate easterDay2016 = LocalDate.of(YEAR, Month.APRIL, EXPECTED_DAY_OF_MONTH);
-  private BankHolidayUtils bankHolidayUtils;
+    private static final int YEAR = 2017;
+    private static final int EXPECTED_DAY_OF_MONTH = 16;
+    private LocalDate easterDay2016 = LocalDate.of(YEAR, Month.APRIL, EXPECTED_DAY_OF_MONTH);
+    private BankHolidayUtils bankHolidayUtils;
 
-  @Before
-  public void setUp() {
-    bankHolidayUtils = new BankHolidayUtils();
-  }
+    @Before
+    public void setUp() {
+        bankHolidayUtils = new BankHolidayUtils();
+    }
 
-  @Test
-  public void getEasterDay() {
-    LocalDate result = this.bankHolidayUtils.getEasterDay(YEAR);
-    assertThat(result).isNotNull().isEqualTo(easterDay2016);
-    result = this.bankHolidayUtils.getEasterDay(2028);
-    assertThat(result).isNotNull().isEqualTo(LocalDate.of(2028, Month.APRIL, 16));
+    @Test
+    public void getEasterDay() {
+        LocalDate result = this.bankHolidayUtils.getEasterDay(YEAR);
+        assertThat(result).isNotNull().isEqualTo(easterDay2016);
+        result = this.bankHolidayUtils.getEasterDay(2028);
+        assertThat(result).isNotNull().isEqualTo(LocalDate.of(2028, Month.APRIL, 16));
 
-    result = this.bankHolidayUtils.getEasterDay(2006);
-    assertThat(result).isNotNull().isEqualTo(LocalDate.of(2006, Month.APRIL, 16));
-  }
+        result = this.bankHolidayUtils.getEasterDay(2006);
+        assertThat(result).isNotNull().isEqualTo(LocalDate.of(2006, Month.APRIL, 16));
+    }
 
-  @Test
-  public void getEasterDayConwayMethod() {
-    LocalDate result = this.bankHolidayUtils.getEasterDayConwayMethod(YEAR);
-    assertThat(result).isNotNull().isEqualTo(easterDay2016);
+    @Test
+    public void getEasterDayConwayMethod() {
+        LocalDate result = this.bankHolidayUtils.getEasterDayConwayMethod(YEAR);
+        assertThat(result).isNotNull().isEqualTo(easterDay2016);
 
-    result = this.bankHolidayUtils.getEasterDayConwayMethod(2028);
-    assertThat(result).isNotNull().isEqualTo(LocalDate.of(2028, Month.APRIL, 16));
+        result = this.bankHolidayUtils.getEasterDayConwayMethod(2028);
+        assertThat(result).isNotNull().isEqualTo(LocalDate.of(2028, Month.APRIL, 16));
 
-    result = this.bankHolidayUtils.getEasterDayConwayMethod(2006);
-    assertThat(result).isNotNull().isEqualTo(LocalDate.of(2006, Month.APRIL, 16));
-  }
+        result = this.bankHolidayUtils.getEasterDayConwayMethod(2006);
+        assertThat(result).isNotNull().isEqualTo(LocalDate.of(2006, Month.APRIL, 16));
+    }
 
-  @Test
-  public void compareMeeusConway() {
-    LocalDate meeus = this.bankHolidayUtils.getEasterDay(YEAR);
-    LocalDate conway = this.bankHolidayUtils.getEasterDayConwayMethod(YEAR);
+    @Test
+    public void compareMeeusConway() {
+        LocalDate meeus = this.bankHolidayUtils.getEasterDay(YEAR);
+        LocalDate conway = this.bankHolidayUtils.getEasterDayConwayMethod(YEAR);
 
-    assertThat(meeus).isNotNull().isEqualTo(easterDay2016);
-    assertThat(conway).isNotNull().isEqualTo(meeus);
-  }
+        assertThat(meeus).isNotNull().isEqualTo(easterDay2016);
+        assertThat(conway).isNotNull().isEqualTo(meeus);
+    }
 
-  @Test
-  public void getMondayOfEasterDay() {
-    LocalDate expected = easterDay2016.plus(1, ChronoUnit.DAYS);
-    LocalDate result = this.bankHolidayUtils.getMondayOfEasterDay(easterDay2016);
-    assertThat(result).isNotNull().isEqualTo(expected);
-  }
+    @Test
+    public void getMondayOfEasterDay() {
+        LocalDate expected = easterDay2016.plus(1, ChronoUnit.DAYS);
+        LocalDate result = this.bankHolidayUtils.getMondayOfEasterDay(easterDay2016);
+        assertThat(result).isNotNull().isEqualTo(expected);
+    }
 
-  @Test
-  public void getAscension() {
-    LocalDate expected = LocalDate.of(YEAR, Month.MAY, 25);
-    LocalDate result = this.bankHolidayUtils.getAscension(easterDay2016);
-    assertThat(result).isNotNull().isEqualTo(expected);
-  }
+    @Test
+    public void getAscension() {
+        LocalDate expected = LocalDate.of(YEAR, Month.MAY, 25);
+        LocalDate result = this.bankHolidayUtils.getAscension(easterDay2016);
+        assertThat(result).isNotNull().isEqualTo(expected);
+    }
 
-  @Test
-  public void getMondayOfPentecote() {
-    LocalDate expected = LocalDate.of(YEAR, Month.JUNE, 5);
-    LocalDate result = this.bankHolidayUtils.getMondayOfPentecote(easterDay2016);
-    assertThat(result).isNotNull().isEqualTo(expected);
-  }
+    @Test
+    public void getMondayOfPentecote() {
+        LocalDate expected = LocalDate.of(YEAR, Month.JUNE, 5);
+        LocalDate result = this.bankHolidayUtils.getMondayOfPentecote(easterDay2016);
+        assertThat(result).isNotNull().isEqualTo(expected);
+    }
 
-  @Test
-  public void isBankHoliday() {
-    // test jour fixe
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JANUARY, 1))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 8))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 1))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JULY, 14))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.AUGUST, 15))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.NOVEMBER, 1))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.NOVEMBER, 11))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.DECEMBER, 25))).isTrue();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.DECEMBER, 31))).isTrue();
+    @Test
+    public void isBankHoliday() {
+        // test jour fixe
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JANUARY, 1))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 8))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 1))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JULY, 14))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.AUGUST, 15))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.NOVEMBER, 1))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.NOVEMBER, 11))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.DECEMBER, 25))).isTrue();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.DECEMBER, 31))).isTrue();
 
-    // pâques
-    assertThat(bankHolidayUtils.isBankHoliday(easterDay2016)).isTrue();
-    // ascension
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 25))).isTrue();
-    // Lundi de pentecote
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JUNE, 5))).isTrue();
+        // pï¿½ques
+        assertThat(bankHolidayUtils.isBankHoliday(easterDay2016)).isTrue();
+        // ascension
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.MAY, 25))).isTrue();
+        // Lundi de pentecote
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.JUNE, 5))).isTrue();
 
-    // test jour non férié
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.FEBRUARY, 14))).isFalse();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.APRIL, 29))).isFalse();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.AUGUST, 11))).isFalse();
-    assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.SEPTEMBER, 24))).isFalse();
-  }
+        // test jour non fï¿½riï¿½
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.FEBRUARY, 14))).isFalse();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.APRIL, 29))).isFalse();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.AUGUST, 11))).isFalse();
+        assertThat(bankHolidayUtils.isBankHoliday(LocalDate.of(2017, Month.SEPTEMBER, 24))).isFalse();
+    }
 }
